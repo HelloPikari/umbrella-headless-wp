9875224b3074e69348b544172b5c7dd1a6ecbf5b
Steve Ariss <steve@arissberg.com>
2021-07-19 16:46:54 -0400

    untrack theme

--- web/app/themes/pikari-headless-wp-theme/template-parts/footer/newsletter-subscribe.php
+++
@@ -1,6 +0,0 @@
-<section class="newsletter-signup container-narrow mx-auto py-16 bg-white">
-
-<?php echo do_shortcode('[wpforms id="3218" title="true" description="false"]'); ?>
-
-</section>
-
--- web/app/themes/pikari-headless-wp-theme/template-parts/header/entry-header.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-?>
-
-<?php the_title( '<h1 class="entry-title text-5xl my-8 text-center sep-center">', '</h1>' ); ?>
